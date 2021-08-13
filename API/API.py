83ad9284d8a56700e8fb47a05ed893b732c2cba9
lrean <64351595+lrean-aily@users.noreply.github.com>
2021-08-13 22:27:14 +0800

    添加邮箱下载提醒和附件推送

--- API/API.py
+++ API/API.py
@@ -7,6 +7,14 @@ import time
 from rich.progress import track
 from rich import print
 from rich.console import Console
+from smtplib import SMTP_SSL
+from email.mime.text import MIMEText
+from email.header import Header
+import _pickle as pk, datetime
+from hashlib import md5
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
+
 
 class Download:
     random_user = None
@@ -21,6 +29,11 @@ class Download:
         self.lastUpdateTime = ""
         self.authorName = ""
         self.isFinish = ""
+        self.type_dict = {
+                1: '玄幻', 2: '奇幻', 3: '武侠', 4: '仙侠', 5: '都市',
+                6: '职场', 7: '历史', 8: '军事', 9: '游戏', 10: '竞技', 
+                11: '科幻', 12: '灵异', 13: '同人', 14: '轻小说'
+                }
         self.headers = {'User-Agent' : None}
         self.USER_AGENT_LIST = [
                 "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.122 UBrowser/4.0.3214.0 Safari/537.36",
@@ -43,9 +56,13 @@ class Download:
                 "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24",
                 "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/535.24 (KHTML, like Gecko) Chrome/19.0.1055.1 Safari/535.24"
             ]
+            
+            
     def user_agent_list(self):
         self.headers['User-Agent'] = random.choice(self.USER_AGENT_LIST)
         return self.headers
+
+
         
         
     def get_bookid(self,bookid):
@@ -92,7 +109,9 @@ class Download:
         novel_intros += '更新:{}\n标签:{}\n'.format(self.lastUpdateTime_chap,self.novel_tag)
         novel_intros += '最后更新章节:{}\n'.format(self.lastUpdateTime)
         novel_intros += '简介信息\n{}'.format(self.novel_intro)
-        print(novel_intros)
+        self.novel_intros = novel_intros
+        print(self.novel_intros)
+        
         with open(os.path.join("config", self.bookName + ".json"), 'r', encoding='utf-8', newline='') as fb:
             chapId_list = fb.read()
         if "简介信息" not in chapId_list:
@@ -112,6 +131,7 @@ class Download:
         catalogue = reps['mixToc']
         number = 0
         self.intro_info()
+        self.send_text()
         for chapters in track(catalogue['chapters']):
             chapters_id = str(chapters['link'])
             with open(os.path.join("config", self.bookName + ".json"), 'r', encoding='utf-8', newline='') as fb:
@@ -126,7 +146,8 @@ class Download:
                 self.write_novel_article(title_body)
             else:
                 continue
-                
+        self.send_file()
+        
         
     def search_book(self):
         self.user_agent_list()
@@ -135,22 +156,23 @@ class Download:
         print(search)
         
 
-    def get_book_list(self):
+    def get_book_list(self, dict_number):
         self.user_agent_list()
         number = 1
         book_url_ID_list = []
         for i in range(10000):
             number += 20
-            url = f"http://api.aixdzs.com/book-sort?gender=6&type=hot&major=%E7%A7%91%E5%B9%BB%C2%B7%E7%81%B5%E5%BC%82&minor=&start={number}&limit=20"
+            url = "http://api.aixdzs.com/book-sort?gender=6&type=hot&major={}&minor=&start={}&limit=20".format(
+                    self.type_dict[int(dict_number)], number)
             novel_info = requests.get(url, headers=self.headers).json()
             for data in novel_info['books']:
-                bookid = data['_id']
+                self.bookid = data['_id']
                 book_title = data['title']
-            book_url_ID_list.append(bookid)
-            with open(os.path.join('novel', f"list.txt"), 'a', newline='') as fb:
-                fb.write(book_url_ID_list)
+            book_url_ID_list.append(self.bookid)
+            # with open(os.path.join('novel', f"list.txt"), 'a', newline='') as fb:
+                # fb.write(book_url_ID_list)
             # print(book_url_ID_list)
-            self.get_bookid(book_url_ID_list)
+            self.get_bookid(self.bookid)
             start = time.time()
             self.download_book()
             end = time.time()
@@ -158,4 +180,72 @@ class Download:
             with open(os.path.join('novel', f"list.txt"), 'a', newline='') as fb:
                 json.dump(novel_info, fb, ensure_ascii=False, indent=4)
                 
+
+
+    def send_text(self):
+        host_server = 'smtp.163.com'
+        pwd = 'ha8277'
+        sender = 'jiang47475tan87@163.com'
+        receiver = '' # input you email
+        mail_content = f"{self.novel_intros}"
+        mail_title = f"开始下载小说:{self.bookName}"
+        msg = MIMEText(mail_content, 'plain', 'utf-8')
+        msg['Subject'] = Header(mail_title, 'utf-8')
+        msg['From'] = sender
+        msg['To'] = receiver
+        try:
+            smtp = SMTP_SSL(host_server)
+            smtp.ehlo(host_server)
+            smtp.set_debuglevel(0)
+            smtp.login(sender, pwd)
+            smtp.sendmail(sender, receiver, msg.as_string())
+            smtp.quit()
+        except smtplib.SMTPException as e:
+            print('error:',e) #打印错误
+ 
+ 
+ 
+    def send_file(self):
+        host_server = 'smtp.163.com'
+        pwd = 'ha8277'
+        sender = 'jiang47475tan87@163.com'
+        receiver = '' # input you email
+        txt_file = os.path.join('novel', f"{self.bookName}.txt")
+        txtApart = MIMEApplication(open(txt_file, 'rb').read())
+        txtApart.add_header('Content-Disposition', 'attachment', filename=f"{self.bookName}.txt")
+        upload_file = MIMEMultipart()
+        upload_file.attach(txtApart)
+        mail_upload_title = f"下载小说文件:{self.bookName}"
+        upload_file['Subject'] = mail_upload_title
+        try:
+            smtp = SMTP_SSL(host_server)
+            smtp.ehlo(host_server)
+            smtp.set_debuglevel(0)
+            smtp.login(sender, pwd)
+            smtp.sendmail(sender, receiver, upload_file.as_string())
+            print("邮件发送完毕")
+            smtp.quit()
+        except smtplib.SMTPException as e:
+            print('error:',e) #打印错误
+
+
+    # def get_type(self):
+        # self.user_agent_list()
+        # type_dict = {}
+        # type_dict_number = 0
+        # get_type_reps = requests.get('http://api.aixdzs.com/sort/lv2', headers=self.headers).json()
+        # for i in get_type_reps['male']:
+            # type_dict_number += 1
+            # major = i['major']
+            # type_dict[type_dict_number] = major
+        # print(type_dict)
+        
+        
+
+
+    
+
+    
+
+
                 
--- API/__init__.py
+++ API/__init__.py
@@ -1,2 +1,7 @@
 from API.API import Download
 import time, re, os, sys
+from smtplib import SMTP_SSL
+from email.mime.text import MIMEText
+from email.header import Header
+import _pickle as pk, datetime
+from hashlib import md5
