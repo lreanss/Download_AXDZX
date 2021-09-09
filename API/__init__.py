866f38cefa6cb83ab2c2cb2b0845b3e6eaac9989
lrean <64351595+lrean-aily@users.noreply.github.com>
2021-09-09 22:00:34 +0800

    文件转移到 单线程.zip 压缩包

--- API/__init__.py
+++
@@ -1,7 +0,0 @@
-from API.API import Download
-import time, re, os, sys
-from smtplib import SMTP_SSL
-from email.mime.text import MIMEText
-from email.header import Header
-import _pickle as pk, datetime
-from hashlib import md5
